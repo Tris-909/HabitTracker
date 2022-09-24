85ab345bffc6a99364fe668cac9c8f763fad77ef
Tri Tran <tranminhtri9090@gmail.com>
2022-09-24 17:13:44 +0700

    Re-structure zustand

--- src/state/index.js
+++ src/state/index.js
@@ -10,12 +10,10 @@ import {
 } from "firebase/firestore";
 
 export const useStore = create((set, get) => ({
-  firestore: {
-    user: {},
-    goals: null,
-  },
+  user: {},
+  // USER
   fetchUser: async (email) => {
-    if (!get().firestore.user?.id) {
+    if (!get().user?.id) {
       const queries = query(
         collection(db, "users"),
         where("email", "==", email)
@@ -30,14 +28,18 @@ export const useStore = create((set, get) => ({
           createdAt: serverTimestamp(),
         });
       }
-      set({ firestore: { user: { id: docs[0]?.id, ...currentUser } } });
+
+      set({
+        user: { id: docs[0]?.id, ...currentUser },
+      });
     }
   },
-  fetchAllGoals: async () => {
-    const userId = get().firestore.user?.id;
-    const goals = get().firestore.goals;
+}));
 
-    if (userId && !goals) {
+export const useGoalStore = create((set, get) => ({
+  goals: [],
+  fetchAllGoals: async ({ userId }) => {
+    if (userId) {
       const queries = query(
         collection(db, "goals"),
         where("parentId", "==", userId)
@@ -49,9 +51,41 @@ export const useStore = create((set, get) => ({
           ...doc.data(),
         };
       });
-      set({ firestore: { ...get().firestore, goals: [...savingGoals] } });
-    } else {
-      set({ firestore: { ...get().firestore, goals: [] } });
+
+      set({ goals: savingGoals });
     }
+  },
+}));
+
+export const useStepStore = create((set, get) => ({
+  steps: [],
+  createStep: async ({ user, amount, description, goalId }) => {
+    const { id, email } = user;
+    const step = {
+      amount: amount,
+      description: description,
+      parentId: goalId,
+      userId: id,
+      createdBy: email,
+      createdAt: serverTimestamp(),
+    };
+    const result = await addDoc(collection(db, "steps"), step);
+
+    set({
+      steps: [...get().steps, { id: result.id, ...step }],
+    });
+  },
+  fetchStepsByGoalId: async ({ goalId }) => {
+    const queries = query(
+      collection(db, "steps"),
+      where("parentId", "==", goalId)
+    );
+    const { docs } = await getDocs(queries);
+    const steps = docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+
+    set({ steps: steps });
   },
 }));
