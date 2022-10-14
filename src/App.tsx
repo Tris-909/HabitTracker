de10a2843ec9c3a322aef0f203748c19915280e5
Tri Tran <tranminhtri9090@gmail.com>
2022-10-14 19:53:19 +0700

    Trying to deploy (1)

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthPage, HomePage } from "./pages";
 import { PrivateRoute } from "components/routes/PrivateRoute";
 import { ChakraProvider } from "@chakra-ui/react";
-import { theme } from "initialization/theme";
+import { theme } from "./initialization/theme";
 import { Toaster } from "react-hot-toast";
 import { AuthContextProvider } from "initialization/firebase";
 import dayjs from "dayjs";
